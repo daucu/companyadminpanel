acdf9984d731f8348b7a61d9817f662021494c4f
info@harshaweb.com <info@harshaweb.com>
2023-02-10 18:56:35 +0530

    auc

--- src/pages/AddAuction.js
+++ src/pages/AddAuction.js
@@ -1,262 +1,65 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import { alpha } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-
-import Tooltip from "@mui/material/Tooltip";
-import { visuallyHidden } from "@mui/utils";
-import FilterListIcon from "@mui/icons-material/FilterList";
-import EditTwoToneIcon from "@mui/icons-material/EditTwoTone";
-import RemoveRedEyeTwoToneIcon from "@mui/icons-material/RemoveRedEyeTwoTone";
-import MuiAlert from "@mui/material/Alert";
-import Snackbar from "@mui/material/Snackbar";
-import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
-import AppBar from "@mui/material/AppBar";
-import AddIcon from "@mui/icons-material/Add";
 import {
-  Dialog,
-  Divider,
-  OutlinedInput,
-  TextareaAutosize,
+  AppBar,
   Button,
-  TextField,
+  Divider,
+  IconButton,
   InputLabel,
-  Select,
-  MenuItem,
+  TextField,
+  Toolbar,
 } from "@mui/material";
-import ModeEditOutlineTwoToneIcon from "@mui/icons-material/ModeEditOutlineTwoTone";
-import { Link as RouterLink, useNavigate } from "react-router-dom";
-// import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
-import { Stack } from "@mui/system";
-const headCells = [
-  {
-    id: "1",
-    numeric: false,
-    disablePadding: true,
-    label: "Name",
-  },
-  {
-    id: "2",
-    numeric: false,
-    disablePadding: true,
-    label: "Description",
-  },
-  {
-    id: "3",
-    numeric: false,
-    disablePadding: false,
-    label: "Published At",
-  },
-  {
-    id: "4",
-    numeric: false,
-    disablePadding: false,
-    label: "Actions",
-  },
-];
+import axios from "axios";
+import React, { useState } from "react";
 
-function EnhancedTableHead(props) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
+function AddAuction() {
+  const [contract, setContract] = useState("");
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState("");
+  const [currency, setCurrency] = useState("");
+  const [minimal_step, setMinimal_step] = useState("");
+  const [description, setDescription] = useState("");
+  const [token, setToken] = useState("token");
+  const [title, setTitle] = useState("");
 
-  const [age, setAge] = React.useState("");
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      contract: contract,
+      value: value,
+      items: items,
+      currency: currency,
+      minimal_step: minimal_step,
+      description: description,
+      token: token,
+      title: title,
+    };
+
+    axios
+      .post(`${process.env.REACT_APP_BACKEND_URL}/bids`, data)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
   return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            sx={
-              {
-                // color: "white"
-              }
-            }
-          />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
-            // sx={{ color: "white" }}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-const EnhancedTableToolbar = (props) => {
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(
-              theme.palette.primary.main,
-              theme.palette.action.activatedOpacity
-            ),
-        }),
-      }}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: "1 1 100%", display: { xs: "none", md: "flex" } }}
-          color="inherit"
-          variant="h6"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{ flex: "1 1 100%", mr: 2, display: { xs: "none", md: "flex" } }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Services Details
-        </Typography>
-      )}
-
-      {/* View Product */}
-      {numSelected === 1 ? (
-        <Tooltip title="View">
-          <IconButton
-            // sx={{ color: "#fff" }}
-            to={`./../view-product/${window.selected}`}
-            component={RouterLink}
-          >
-            <RemoveRedEyeTwoToneIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        ""
-      )}
-
-      {/* Edit Product */}
-      {numSelected === 1 ? (
-        <Tooltip
-          title="Edit"
-          // sx={{ color: "#fff" }}
-        >
-          <IconButton
-            to={`./../update-product/${window.selected}`}
-            component={RouterLink}
-          >
-            <EditTwoToneIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        ""
-      )}
-
-      {/* Delete Product */}
-      {numSelected > 0 ? (
-        <Tooltip title="Delete" sx={{}}>
-          <IconButton onClick={window.deleteProduct}>
-            <DeleteTwoToneIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-};
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
-
-export default function AddAuction() {
-  const [age, setAge] = React.useState("");
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-
-  const navigate = useNavigate();
-  // Alert
-  const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
-
-  return (
-    <Box
-      sx={{
-        width: "100%",
-        marginTop: 3,
-        boxShadow: 0,
-        animation: "fadeIn 0.5s ease-in-out",
-        transition: "box-shadow 1s ease-in-out",
+    <div
+      style={{
+        marginTop: "20px",
       }}
     >
       <AppBar position="static">
-        <Toolbar variant="dense" sx={{ background: "#333", color: "#fff" }}>
-          <Typography variant="h6" color="inherit" component="div">
-            Create Auctions
-          </Typography>
+        <Toolbar variant="dense" sx={{ background: "#333" }}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          ></IconButton>
+          Add Auction
           <Divider sx={{ flexGrow: 1 }} />
           <Button
             variant="contained"
@@ -265,88 +68,255 @@ export default function AddAuction() {
             sx={{
               boxShadow: 0,
             }}
+            onClick={handleSubmit}
           >
             Publish
           </Button>
         </Toolbar>
       </AppBar>
-      <div>
-        {/* title */}
-        <TextField
-          id="outlined-basic"
-          label="Title"
-          size="small"
-          variant="outlined"
-          sx={{ width: "100%", marginTop: 2 }}
-        />
-        {/* Value */}
-        <TextField
-          id="outlined-basic"
-          label="Value"
-          size="small"
-          variant="outlined"
-          sx={{ width: "100%", marginTop: 2 }}
-        />
-        {/* local terms */}
-
-        <div>
-          <TextField
-            id="outlined-basic"
-            label="Currency"
-            size="small"
-            variant="outlined"
-            sx={{ width: "100%", marginTop: 2 }}
-          />
-        </div>
-        <div>
-          <TextField
-            id="outlined-basic"
-            label="Descreption"
-            size="small"
-            variant="outlined"
-            sx={{ width: "100%", marginTop: 2 }}
-          />
-        </div>
-        <div>
-          <TextField
-            id="outlined-basic"
-            label="Minimal Step"
-            size="small"
-            variant="outlined"
-            sx={{ width: "100%", marginTop: 2 }}
-          />
-        </div>
-        <div>
-          <InputLabel
-            id="demo-simple-select-label"
-            sx={{
-              width: "100%",
-              marginTop: 2,
-              textAlign: "left",
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div
+          style={{
+            marginTop: "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "25%",
+              padding: "10px",
+              display: "flex",
+              alignItems: "center",
             }}
           >
-            Contract
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={age}
-            size="small"
-            label="Age"
-            defaultChecked="Select Contract"
-            sx={{
-              width: "100%",
-              marginTop: 1,
-              textAlign: "left",
+            <InputLabel htmlFor="outlined-adornment-amount">
+              Contract
+            </InputLabel>
+          </div>
+          <div
+            style={{
+              width: "75%",
             }}
-            onChange={handleChange}
           >
-            <MenuItem value={10}>Select Contract</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
+            <TextField
+              id="outlined-basic"
+              label="Contract"
+              name={contract}
+              onChange={(e) => setContract(e.target.value)}
+              size="small"
+              variant="outlined"
+              sx={{ width: "100%", marginTop: 2 }}
+            />
+          </div>
         </div>
-      </div>
-    </Box>
+        <div
+          style={{
+            marginTop: "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "25%",
+              padding: "10px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <InputLabel htmlFor="outlined-adornment-amount">Value</InputLabel>
+          </div>
+          <div
+            style={{
+              width: "75%",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Value"
+              name={value}
+              onChange={(e) => setValue(e.target.value)}
+              size="small"
+              variant="outlined"
+              sx={{ width: "100%", marginTop: 1 }}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            marginTop: "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "25%",
+              padding: "10px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <InputLabel htmlFor="outlined-adornment-amount">Items</InputLabel>
+          </div>
+          <div
+            style={{
+              width: "75%",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Items"
+              size="small"
+              name={items}
+              onChange={(e) => setItems(e.target.value)}
+              variant="outlined"
+              sx={{ width: "100%", marginTop: 1 }}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            marginTop: "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "25%",
+              padding: "10px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <InputLabel htmlFor="outlined-adornment-amount">
+              Currency
+            </InputLabel>
+          </div>
+          <div
+            style={{
+              width: "75%",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Currency"
+              size="small"
+              name={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+              variant="outlined"
+              sx={{ width: "100%", marginTop: 1 }}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            marginTop: "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "25%",
+              padding: "10px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <InputLabel htmlFor="outlined-adornment-amount">
+              Minimal_step
+            </InputLabel>
+          </div>
+          <div
+            style={{
+              width: "75%",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Minimal_step"
+              size="small"
+              name={minimal_step}
+              onChange={(e) => setMinimal_step(e.target.value)}
+              variant="outlined"
+              sx={{ width: "100%", marginTop: 1 }}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            marginTop: "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "25%",
+              padding: "10px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <InputLabel htmlFor="outlined-adornment-amount">
+              Description
+            </InputLabel>
+          </div>
+          <div
+            style={{
+              width: "75%",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Description"
+              size="small"
+              name={description}
+              onChange={(e) => setDescription(e.target.value)}
+              variant="outlined"
+              sx={{ width: "100%", marginTop: 1 }}
+            />
+          </div>
+        </div>
+        <div
+          style={{
+            marginTop: "10px",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "25%",
+              padding: "10px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <InputLabel htmlFor="outlined-adornment-amount">Title</InputLabel>
+          </div>
+          <div
+            style={{
+              width: "75%",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Title"
+              size="small"
+              name={title}
+              onChange={(e) => setTitle(e.target.value)}
+              variant="outlined"
+              sx={{ width: "100%", marginTop: 1 }}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
+
+export default AddAuction;
